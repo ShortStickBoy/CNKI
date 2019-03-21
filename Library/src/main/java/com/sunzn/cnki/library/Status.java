0cf0914452bd1be6a292a3e55d9a9210898521c4
sunzn <sunzn.cn@gmail.com>
2019-03-21 14:50:34 +0800

    Add Status

---
+++ Library/src/main/java/com/sunzn/cnki/library/Status.java
@@ -0,0 +1,12 @@
+package com.sunzn.cnki.library;
+
+public class Status {
+
+    public class Page {
+        public static final int Process = 0;    // 加载
+        public static final int Success = 1;    // 成功
+        public static final int Failure = 2;    // 失败
+        public static final int Nothing = 3;    // 空白
+    }
+
+}
