38662f3f04b4ae0cbda0f032744c5effe0d7539b
sunzn <sunzn.cn@gmail.com>
2019-03-27 17:07:16 +0800

    Add Format

---
+++ Library/src/main/java/com/sunzn/cnki/library/Format.java
@@ -0,0 +1,9 @@
+package com.sunzn.cnki.library;
+
+public class Format {
+
+    public static final String CAJ = "caj";
+    public static final String PDF = "pdf";
+    public static final String EPB = "epub";
+
+}
