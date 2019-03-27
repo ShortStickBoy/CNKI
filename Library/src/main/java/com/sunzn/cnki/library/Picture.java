53f0dc96e6503c27bcf4514e8fac5570d0266166
sunzn <sunzn.cn@gmail.com>
2019-03-27 10:01:18 +0800

    Update Picture

--- Library/src/main/java/com/sunzn/cnki/library/Picture.java
+++ Library/src/main/java/com/sunzn/cnki/library/Picture.java
@@ -2,18 +2,23 @@ package com.sunzn.cnki.library;
 
 public class Picture {
 
-    private static final String ROOT = "http://c61.cnki.net/CJFD/big/";
+    private static final String BZ_ROOT = "http://xyz.cnki.net/newspaper/";
+    private static final String QK_ROOT = "http://c61.cnki.net/CJFD/big/";
+
+    public static String getNewsPaperCover(String code) {
+        return BZ_ROOT + code + ".png";
+    }
 
     public static String getJournalMainCover(String code) {
-        return ROOT + code + ".jpg";
+        return QK_ROOT + code + ".jpg";
     }
 
     public static String getJournalTermCover(String code, String term) {
-        return ROOT + code + "/" + code + term + ".jpg";
+        return QK_ROOT + code + "/" + code + term + ".jpg";
     }
 
     public static String getJournalTermCover(String code, String year, String issue) {
-        return ROOT + code + "/" + code + year + issue + ".jpg";
+        return QK_ROOT + code + "/" + code + year + issue + ".jpg";
     }
 
     public static String getPinDeCover(String code) {
