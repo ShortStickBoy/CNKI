090ce560fab27bb3b65eb068bb5547b43412ad9a
sunzn <sunzn.cn@gmail.com>
2019-03-28 13:46:21 +0800

    Add Token

--- Library/src/main/java/com/sunzn/cnki/library/Token.java
+++ Library/src/main/java/com/sunzn/cnki/library/Token.java
@@ -2,7 +2,7 @@ package com.sunzn.cnki.library;
 
 public class Token {
 
-    public static final String AA001 = "AA001";     // 期刊文献 * 节点 * 单篇
+    public static final String AA001 = "AA001";     // 期刊文献 - 节点 - 单篇
     public static final String AB001 = "AB001";     // 博士论文 * 节点 * 单篇
     public static final String AC001 = "AC001";     // 硕士论文（节点-单篇）
     public static final String AD001 = "AD001";     // 会议论文（节点-单篇）
