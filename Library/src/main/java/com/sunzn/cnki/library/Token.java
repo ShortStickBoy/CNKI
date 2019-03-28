8cf0d99a837ee0723b506c8c07eb56abe0610562
sunzn <sunzn.cn@gmail.com>
2019-03-28 14:24:23 +0800

    Add Token

--- Library/src/main/java/com/sunzn/cnki/library/Token.java
+++ Library/src/main/java/com/sunzn/cnki/library/Token.java
@@ -2,10 +2,20 @@ package com.sunzn.cnki.library;
 
 public class Token {
 
-    public static final String AA001 = "AA001";     // 期刊文献 - 节点 - 单篇
-    public static final String AB001 = "AB001";     // 博士论文 - 节点 - 单篇
-    public static final String AC001 = "AC001";     // 硕士论文 - 节点 - 单篇
-    public static final String AD001 = "AD001";     // 会议论文 - 节点 - 单篇
-    public static final String AE001 = "AE001";     // 报纸文献 - 节点 - 单篇
+    public static final String AA001 = "AA001";     // 期刊文献 - 文献 - 单篇
+    public static final String AB001 = "AB001";     // 博士论文 - 文献 - 单篇
+    public static final String AC001 = "AC001";     // 硕士论文 - 文献 - 单篇
+    public static final String AD001 = "AD001";     // 会议论文 - 文献 - 单篇
+    public static final String AE001 = "AE001";     // 报纸文献 - 文献 - 单篇
+
+    public static final String BA001 = "BA001";     // 期刊详情 - 详情 - 期刊
+    public static final String BB001 = "BB001";     // 期刊主页 - 主页 - 期刊
+    public static final String BC001 = "BC001";     // 期刊频道 - 频道 - 期刊
+    public static final String BD001 = "BD001";     // 一级频道 - 频道 - 期刊
+    public static final String BE001 = "BE001";     // 热门推荐 - 栏目 - 期刊
+    public static final String BE002 = "BE002";     // 周排行榜 - 栏目 - 期刊
+    public static final String BE003 = "BE003";     // 重磅推荐 - 栏目 - 期刊
+    public static final String BE004 = "BE004";     // 新刊速递 - 栏目 - 期刊
+    public static final String BE005 = "BE005";     // 免费体验 - 栏目 - 期刊
 
 }
