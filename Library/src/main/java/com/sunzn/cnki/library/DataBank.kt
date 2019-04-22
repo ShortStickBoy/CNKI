fa301bb8c7bd98648e767abe2998d5a8063cb69f
sunzn <sunzn.cn@gmail.com>
2019-04-22 15:54:45 +0800

    Add DataBank

---
+++ Library/src/main/java/com/sunzn/cnki/library/DataBank.kt
@@ -0,0 +1,14 @@
+package com.sunzn.cnki.library
+
+object DataBank {
+
+    const val 期刊 = "cjfdtotal"
+    const val 报纸 = "ccndtotal"
+    const val 会议 = "cpfdtotal"
+    const val 博士 = "cdfdtotal"
+    const val 硕士 = "cmfdtotal"
+    const val 论文 = "cmfdtotal,cdmdtotal"
+    const val 除优 = "cjfdtotal_o"
+    const val 学者 = "cnki_scholar"
+
+}
