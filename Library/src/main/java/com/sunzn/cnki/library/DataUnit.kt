287e1862818285c8e997947e627929e25a69fc82
sunzn <sunzn.cn@gmail.com>
2019-06-26 15:45:37 +0800

    Add DataUnit

---
+++ Library/src/main/java/com/sunzn/cnki/library/DataUnit.kt
@@ -0,0 +1,16 @@
+package com.sunzn.cnki.library
+
+class DataUnit {
+
+    class DIAN {
+        companion object {
+            const val PTH0100 = "PTH0100"  // 图文横线
+            const val PTV0100 = "PTV0100"  // 图文纵向
+            const val ADH0100 = "ADH0100"  // 音频横向
+            const val VDV0100 = "VDV0100"  // 视频纵向
+            const val ATH0100 = "ATH0100"  // 图集横向
+            const val MPL0100 = "MPL0100"  // 微刊集合
+        }
+    }
+
+}
