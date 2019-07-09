a4b22eb533be00b782b661d91f69c400f7e1cea2
sunzn <sunzn.cn@gmail.com>
2019-07-09 17:02:03 +0800

    Update DataUnit

--- Library/src/main/java/com/sunzn/cnki/library/DataUnit.kt
+++ Library/src/main/java/com/sunzn/cnki/library/DataUnit.kt
@@ -4,7 +4,9 @@ class DataUnit {
 
     class DIAN {
         companion object {
-            const val PTH0100 = "PTH0100"  // 图文横线
+            const val MDY0100 = "MDY0100"  // 每日典言
+            const val MES0100 = "MES0100"  // 早晚故事
+            const val PTH0100 = "PTH0100"  // 图文横向
             const val PTV0100 = "PTV0100"  // 图文纵向
             const val ADH0100 = "ADH0100"  // 音频横向
             const val VDV0100 = "VDV0100"  // 视频纵向
