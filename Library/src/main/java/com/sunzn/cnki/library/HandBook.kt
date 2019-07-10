d829d2deb747d1102405a88fad730e1ce2f48f09
sunzn <sunzn.cn@gmail.com>
2019-07-10 15:07:32 +0800

    Add HandBook

---
+++ Library/src/main/java/com/sunzn/cnki/library/HandBook.kt
@@ -0,0 +1,12 @@
+package com.sunzn.cnki.library
+
+class HandBook {
+
+    class Sort {
+        companion object {
+            const val COVER = "Cover"    // 封面
+            const val ENTRY = "Entry"    // 词条
+        }
+    }
+
+}
