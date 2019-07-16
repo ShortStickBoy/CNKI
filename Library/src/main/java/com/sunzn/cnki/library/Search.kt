35abec37adf5814929093a1c95a88a671d903d05
sunzn <sunzn.cn@gmail.com>
2019-07-16 16:34:31 +0800

    Add Search

---
+++ Library/src/main/java/com/sunzn/cnki/library/Search.kt
@@ -0,0 +1,13 @@
+package com.sunzn.cnki.library
+
+class Search {
+
+    class HandBook {
+        companion object {
+            const val Complex = "1000"        // 综合排序
+            const val TermNum = "3000"        // 词条数量
+            const val PubDate = "2000"        // 出版时间
+        }
+    }
+
+}
