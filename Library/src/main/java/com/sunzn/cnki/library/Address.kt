635ef536424265b692011c84f6c002c3cdae13dd
sunzn <sunzn.cn@gmail.com>
2019-09-18 18:55:41 +0800

    Update Address

--- Library/src/main/java/com/sunzn/cnki/library/Address.kt
+++ Library/src/main/java/com/sunzn/cnki/library/Address.kt
@@ -22,4 +22,59 @@ object Address {
         }
     }
 
+    /**
+     * 分享-期刊节点
+     */
+    fun getJournalNodeShareUrl(code: String?): String {
+        return when (code) {
+            null -> Values.EMPTY
+            else -> "https://wap.cnki.net/touch/web/Journal/Article/$code.html"
+        }
+    }
+
+    /**
+     * 分享-报纸节点
+     */
+    fun getNewspaperNodeShareUrl(code: String?): String {
+        return when (code) {
+            null -> Values.EMPTY
+            else -> "https://wap.cnki.net/touch/web/Newspaper/Article/$code.html"
+        }
+    }
+
+    /**
+     * 分享-会议节点
+     */
+    fun getConferenceNodeShareUrl(code: String?): String {
+        return when (code) {
+            null -> Values.EMPTY
+            else -> "https://wap.cnki.net/touch/web/Conference/Article/$code.html"
+        }
+    }
+
+    /**
+     * 分享-论文节点
+     */
+    fun getDissertationNodeShareUrl(code: String?): String {
+        return when (code) {
+            null -> Values.EMPTY
+            else -> "https://wap.cnki.net/touch/web/Dissertation/Article/$code.html"
+        }
+    }
+
+    /**
+     * 协议-隐私政策
+     */
+    fun getLegalUrl(): String {
+        return "https://wap.cnki.net/touch/web/appzhengce.html"
+    }
+
+    /**
+     * 协议-帮助中心
+     */
+    fun getHelpUrl(): String {
+        return "https://wap.cnki.net/client.html"
+    }
+
 }
+
