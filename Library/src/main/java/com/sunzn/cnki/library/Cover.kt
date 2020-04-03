45e3e85e88caa8dbbfb4f01bacd7ad6c96247f9c
sunzn <sunzn.cn@gmail.com>
2020-04-03 10:22:20 +0800

    Add Cover

---
+++ Library/src/main/java/com/sunzn/cnki/library/Cover.kt
@@ -0,0 +1,39 @@
+package com.sunzn.cnki.library
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import java.io.File
+import java.io.FileOutputStream
+
+object Cover {
+
+    fun gainCover(context: Context, name: String): File {
+        return File(context.getDir("cover", Context.MODE_PRIVATE), name)
+    }
+
+    fun saveCover(context: Context, drawable: Drawable, name: String) {
+        val file: File = File(context.getDir("cover", Context.MODE_PRIVATE), name)
+        saveCover(drawable, file, Bitmap.CompressFormat.JPEG)
+    }
+
+    fun saveCover(drawable: Drawable, file: File, format: Bitmap.CompressFormat) {
+        var out: FileOutputStream? = null
+        try {
+            if (file.exists()) {
+                file.delete()
+                file.createNewFile()
+            }
+            out = FileOutputStream(file)
+            if (drawable is BitmapDrawable) {
+                drawable.bitmap.compress(format, 100, out)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            out?.close()
+        }
+    }
+
+}
