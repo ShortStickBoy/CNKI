677518b3d7d3854ac427488841b6467e4a016ec2
sunzn <sunzn.cn@gmail.com>
2021-03-17 09:06:45 +0800

    Update Format

--- Library/src/main/java/com/sunzn/cnki/library/Format.kt
+++ Library/src/main/java/com/sunzn/cnki/library/Format.kt
@@ -4,6 +4,8 @@ object Format {
 
     const val CAJ = "caj"
     const val PDF = "pdf"
+    const val KDH = "kdh"
+    const val NH = "nh"
     const val EPB = "epub"
     const val MP3 = "mp3"
     const val MP4 = "mp4"
